4575e5dfc476c466603e793e224fe4c14fc4ec84
Abdulaziz <208419216+Abdulaziz-Alrashidi@users.noreply.github.com>
2025-11-07 18:53:36 +0300

    Finalized pretraining model prototype 3 architecture

---
+++ src/pretraining_model_prototype_3.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+
+def build_model_v2(shape=(64, 64, 3), rescaling=(1.0 / 255), optimizer="adam"):
+    model = tf.keras.Sequential([
+        #Input, Augmentation and Scaling
+        tf.keras.layers.Input(shape=(64, 64, 3)),
+        tf.keras.layers.Rescaling(rescaling),
+
+        #Block 1
+        tf.keras.layers.Conv2D(16, (2, 2), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        tf.keras.layers.Conv2D(16, (2, 2), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        tf.keras.layers.Conv2D(16, (2, 2), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        tf.keras.layers.Conv2D(16, (2, 2), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        tf.keras.layers.AveragePooling2D((2, 2)),
+
+        #Block 2
+        tf.keras.layers.Conv2D(32, (3, 3), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #compress
+        tf.keras.layers.Conv2D(16, (1, 1), padding="same"),
+        tf.keras.layers.Conv2D(32, (3, 3), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #expand
+        tf.keras.layers.Conv2D(32, (1, 1), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #compress
+        tf.keras.layers.Conv2D(16, (1, 1), padding="same"),
+        tf.keras.layers.Conv2D(32, (3, 3), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #expand
+        tf.keras.layers.Conv2D(32, (1, 1), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        tf.keras.layers.AveragePooling2D((2, 2)),
+
+        #Block 3
+        tf.keras.layers.Conv2D(64, (3, 3), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #compress
+        tf.keras.layers.Conv2D(32, (1, 1), padding="same"),
+        tf.keras.layers.Conv2D(64, (3, 3), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #expand
+        tf.keras.layers.Conv2D(64, (1, 1), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #compress
+        tf.keras.layers.Conv2D(32, (1, 1), padding="same"),
+        tf.keras.layers.Conv2D(64, (3, 3), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #expand
+        tf.keras.layers.Conv2D(64, (1, 1), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        tf.keras.layers.AveragePooling2D((2, 2)),
+
+        #Block 4
+        tf.keras.layers.Conv2D(128, (3, 3), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #compress
+        tf.keras.layers.Conv2D(64, (1, 1), padding="same"),
+        tf.keras.layers.Conv2D(128, (3, 3), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #expand
+        tf.keras.layers.Conv2D(128, (1, 1), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #compress
+        tf.keras.layers.Conv2D(64, (1, 1), padding="same"),
+        tf.keras.layers.Conv2D(128, (3, 3), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        #expand
+        tf.keras.layers.Conv2D(128, (1, 1), padding="same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        tf.keras.layers.AveragePooling2D((2, 2)),
+
+        #Block 5
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(400),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        tf.keras.layers.Dropout(0.35),
+        tf.keras.layers.Dense(300),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LeakyReLU(),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(200),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Softmax(),
+
+
+    ])
+    model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
+    return model
